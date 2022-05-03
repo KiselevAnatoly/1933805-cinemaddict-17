97120e26777f1dfe0d364be2fd6b74925a1e6a3d
Anatoly <akiselev57@mail.ru>
2022-05-03 11:50:41 +0300

    1.9. Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 
+import Filters from './view/filters.js';
+import {render} from './render.js';
+import MoveCards from './view/movie-card.js';
+import  MovieDetails from './view/movie-details.js';
+//import ShowMoreButton from './view/show-more.js';
+
+
+const siteMainElement = document.querySelector('.main');
+
+
+render(new Filters(), siteMainElement);
+render(new MoveCards(),siteMainElement);
+render(new MovieDetails(),siteMainElement);
+//render(new ShowMoreButton(),siteMainElement);
---
+++ src/view/filters.js
@@ -0,0 +1,29 @@
+import { createElement } from '../render.js';
+
+const createNewTaskButtonTemplate = () => `<nav class="main-navigation">
+<a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+<a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+<a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+<a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+</nav>`;
+
+export default class Filters {
+  // Получить шаблон разметки.
+  getTemplate() {
+    return createNewTaskButtonTemplate();
+  }
+
+  //На основе разметки создать DOM элемент.
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  // Удалить элемент.
+  removeElement() {
+    this.element = null;
+  }
+}
