8e7ff33f7f97af91402ee12503f4b790e2c6d209
Anatoly <akiselev57@mail.ru>
2022-05-06 14:17:14 +0300

    module1-task3

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -11,13 +11,14 @@ export default class MoviesPresenter {
     this.filmsContainer  = filmsContainer ;
     const movieList = new MovieDetails();
     const filmsListContainer = movieList.getElement().querySelector('.films-list__container');
+    const QUANTITY_CARDS = 5;
     render(new Filters(), this.filmsContainer );
     render(new MovieSort(), this.filmsContainer );
     render(movieList, this.filmsContainer );
     render(new ShowMore(), this.filmsContainer );
 
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < QUANTITY_CARDS; i++) {
       render(new FilmCard(),filmsListContainer);
     }
 
