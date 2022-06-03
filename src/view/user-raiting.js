1da4ada6a8b0c99824c76bb4680db185e67a35fe
Anatoly <akiselev57@mail.ru>
2022-06-03 12:42:42 +0300

    3.9. Революция или эволюция? (часть 2)/2

--- src/view/user-raiting.js
+++ src/view/user-raiting.js
@@ -2,7 +2,7 @@ import View from './view-class';
 
 const createRatingTemplate = () =>
   `<section class="header__profile profile">
-  <p class="profile__rating">Movie Buff</p>
+  <p class="profile__rating">Anatoly</p>
   <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
 </section>`;
 
