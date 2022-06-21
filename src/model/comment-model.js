fab22f689c5338b220e31ed50979bddbf0ddddb7
Anatoly <akiselev57@mail.ru>
2022-06-21 10:39:44 +0300

    Меняй-удаляй

--- src/model/comment-model.js
+++ src/model/comment-model.js
@@ -1,11 +1,28 @@
 import { generateComment } from '../mock/comments';
 import Observable from '../framework/observable';
-import { COMMENTS_COUNT } from './model/movie-card-model';
 
-export default class MovieModel extends Observable {
+const COMMENTS_COUNT = 57;
+
+export default class CommentModel extends Observable {
   #comments = Array.from({ length: COMMENTS_COUNT }, generateComment);
 
-  get comments() {
+  get comments () {
     return this.#comments;
   }
+
+  set comments(comments) {
+    this.#comments = comments;
+  }
+
+  deleteComment = (updateType, id) => {
+    this.#comments = this.#comments.filter((comment) => comment.id !== id);
+
+    this._notify(updateType, id);
+  };
+
+  addComment = (updateType, update) => {
+    this.#comments.push(update);
+
+    this._notify(updateType, update);
+  };
 }
