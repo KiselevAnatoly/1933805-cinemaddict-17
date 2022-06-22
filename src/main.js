45330fc8b6757e13792b7b749dbaa39a76281b8c
Anatoly <akiselev57@mail.ru>
2022-06-22 22:06:49 +0300

    Пришёл, увидел, загрузил (часть 2)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 
-import {render} from './framework/render';
-import StatisticsView from './view/statistics-view';
 import FilmSectionPresenter from './presenter/movie-presenter';
 import FilterPresenter from './presenter/filter-presenter';
 import UserProfilePresenter from './presenter/user-profile-presenter';
 import MovieModel from './model/movie-card-model';
 import FilterModel from './model/filter-model';
 import FilmsApiService from './api-service';
-import { UpdateType } from './const';
 
 
 const AUTHORIZATION = 'Basic Anatoly';
@@ -15,7 +12,6 @@ const END_POINT = 'https://17.ecmascript.pages.academy/cinemaddict';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
-const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
 const filmApiService = new FilmsApiService(END_POINT, AUTHORIZATION);
 const filmsModel = new MovieModel(filmApiService);
 const filterModel = new FilterModel();
@@ -29,9 +25,4 @@ contentPresenter.init();
 filmsModel.init();
 userProfilePresenter.init();
 
-filmsModel.addObserver((updateType) => {
-  if (updateType === UpdateType.INIT) {
-    render(new StatisticsView(filmsModel.films), siteFooterStatisticsElement);
 
-  }
-});
